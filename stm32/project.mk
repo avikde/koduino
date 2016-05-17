829e6cf99139fe466d8755e68cbd594f5f0c1139
Avik De <avikde@gmail.com>
2016-05-17 09:27:39 -0400

    removed percents from project.mk upload flash and ram

--- stm32/project.mk
+++ stm32/project.mk
@@ -169,7 +169,7 @@ $(BUILDDIR)/$(PROJNAME).bin: $(OBJS)
 	@echo "[LD] $(@F)"
 	@$(LD) $(LDFLAGS) $(OBJS) $(KODUINO_DIR)/system/lib$(SERIES).a -o $(BUILDDIR)/$(PROJNAME).elf $(LDFLAGSMCU)
 	@arm-none-eabi-objcopy --output-target=binary $(BUILDDIR)/$(PROJNAME).elf $(BUILDDIR)/$(PROJNAME).bin
-	@$(SZ) $(BUILDDIR)/$(PROJNAME).elf | tail -n 1 | awk '{print "Flash:\t", $$1+$$2, "\t[",($$1+$$2)*100/(256*1024),"% ]", "\nRAM:\t", $$2+$$3, "\t[",($$2+$$3)*100/(32*1024),"% ]"}'
+	@$(SZ) $(BUILDDIR)/$(PROJNAME).elf | tail -n 1 | awk '{print "Flash: ", $$1+$$2, ", RAM: ", $$2+$$3}'
 
 disassemble: $(BUILDDIR)/$(PROJNAME).bin
 	@$(OBJDMP) -x --syms $(PREF)$< > $(BUILDDIR)/$(PROJNAME).dmp
